6a364af0cccb6d13eee179d129e65b70b9ca9723
KairaveeR <r.kairavee@gmail.com>
2023-08-17 11:44:57 +0530

    lot of changes added many files

---
+++ src/notauthorized.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function NotFound() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="headingwriting">
+            you are not authorized to access this page
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default NotAuthorized;
